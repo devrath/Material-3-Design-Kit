0bb810d25d1b88cced0cc8a32af97a6404a927c8
Devrath <devrath.dev595@gmail.com>
2023-02-12 19:04:33 +0530

    more colors added

--- Code/app/src/main/java/com/istudio/materialapp/ui/theme/Theme.kt
+++ Code/app/src/main/java/com/istudio/materialapp/ui/theme/Theme.kt
@@ -25,34 +25,49 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun MaterialAppTheme(
+    // Flag to determine the dark/light theme
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
+    // Parent most top level composable
     content: @Composable () -> Unit
 ) {
+   // We need context here because the support for dynamic colors comes from android system
+    val context = LocalContext.current
+    val view = LocalView.current
+
+    // We can apply one of four different color scheme's depending on what type of device the user has.
+    // CONDITION-1:-> If the user is running on android-12 and above, We need to use dynamic colors.
+    // CONDITION-2:-> If the user is running on device prior to android-12, We will use normal material-2 theme.
+    // CONDITION-3:-> If the user again is using light theme, then we use light colors else use dark colors.
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            // CONDITION-1:-> If the user is running on android-12 and above, We need to use dynamic colors.
+            if (darkTheme){
+                // CONDITION-3:-> If the user again is using dark theme, then we use dark colors.
+                dynamicDarkColorScheme(context)
+            } else {
+                // CONDITION-3:-> If the user again is using light theme, then we use light colors.
+                dynamicLightColorScheme(context)
+            }
         }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> {
+            // CONDITION-1:-> If the user is running on below android-12, We need to use material-2 colors.
+            // CONDITION-3:-> If the user again is using dark theme, then we use dark colors.
+            DarkColorScheme
+        }
+        else -> {
+            // CONDITION-1:-> If the user is running on below android-12, We need to use material-2 colors.
+            // CONDITION-3:-> If the user again is using light theme, then we use light colors.
+            LightColorScheme
+        }
     }
-    val view = LocalView.current
+
+    // Set the color for the status bar
     if (!view.isInEditMode) {
         SideEffect {
             (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
