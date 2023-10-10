fb642e10f3c65118eef2543780ed1973ab5748ab
devrath dev <devrath.dev595@gmail.com>
2023-10-10 19:34:10 +0530

    upgrading dependencies

---
+++ Code/app/src/main/java/com/istudio/materialapp/modules/ui_buttons/ButtonsActivity.kt
@@ -0,0 +1,46 @@
+package com.istudio.materialapp.modules.ui_buttons
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.istudio.materialapp.ui.theme.MaterialAppTheme
+
+class ButtonsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ScreenContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
+}
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MaterialAppTheme {
+        ScreenContent()
+    }
+}
