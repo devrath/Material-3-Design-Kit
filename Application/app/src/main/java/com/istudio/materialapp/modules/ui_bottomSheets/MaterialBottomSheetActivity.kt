f8cf3935b0898e2a5087d0f84e0c00828b90b4ec
devrath dev <devrath.dev595@gmail.com>
2023-10-15 21:24:41 +0530

    Bottom sheet code added

--- Application/app/src/main/java/com/istudio/materialapp/modules/ui_bottomSheets/MaterialBottomSheetActivity.kt
+++ Application/app/src/main/java/com/istudio/materialapp/modules/ui_bottomSheets/MaterialBottomSheetActivity.kt
@@ -4,15 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.istudio.materialapp.ui.theme.MaterialAppTheme
+import kotlinx.coroutines.launch
 
 class MaterialBottomSheetActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +50,7 @@ class MaterialBottomSheetActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
     Column(
@@ -39,6 +59,59 @@ fun ScreenContent(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
     ){
 
+        val sheetState = rememberModalBottomSheetState()
+        var isSheetOpen by rememberSaveable { mutableStateOf(false) }
+        val coroutineScope = rememberCoroutineScope()
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Button(onClick = {
+                coroutineScope.launch {
+                    isSheetOpen = true
+                    //sheetState.expand()
+                }
+            }) {
+                Text(text = "Launch Bottom Sheet")
+            }
+        }
+        if(isSheetOpen){
+            ModalBottomSheet(
+                sheetState = sheetState,
+                onDismissRequest = {
+                    coroutineScope.launch {
+                        isSheetOpen = false
+                        sheetState.hide()
+                    }
+                }
+            ) {
+                BottomSheetContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomSheetContent() {
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(10){currentNumber ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(vertical = 25.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Current Row -> $currentNumber",
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+        }
     }
 }
 
