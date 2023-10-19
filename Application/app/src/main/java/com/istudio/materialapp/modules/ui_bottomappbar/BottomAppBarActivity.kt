20322ae2fe304baafb8d67f38b7b932f8a2c543c
DevIndiGG <devrath.ad@indi.gg>
2023-10-19 10:55:58 +0530

    more code added

---
+++ Application/app/src/main/java/com/istudio/materialapp/modules/ui_bottomappbar/BottomAppBarActivity.kt
@@ -0,0 +1,116 @@
+package com.istudio.materialapp.modules.ui_bottomappbar
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAPhoto
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.DarkMode
+import androidx.compose.material.icons.filled.Landscape
+import androidx.compose.material.icons.filled.Layers
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.istudio.materialapp.ui.theme.MaterialAppTheme
+
+class BottomAppBarActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ScreenContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+        val cxt = LocalContext.current
+
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            bottomBar = {
+                BottomAppBar(actions = {
+                    IconButton(onClick = {
+                        Toast.makeText(cxt, "Landscape icon selected", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Landscape,
+                            contentDescription = "Landscape"
+                        )
+                    }
+                    IconButton(onClick = {
+                        Toast.makeText(cxt, "Layers icon selected", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Layers,
+                            contentDescription = "Layers"
+                        )
+                    }
+                    IconButton(onClick = {
+                        Toast.makeText(cxt, "Dark Mode icon selected", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.DarkMode,
+                            contentDescription = "Dark Mode"
+                        )
+                    }
+                })
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.AddCircle,
+                        contentDescription = "Call contact"
+                    )
+                }
+            }
+        ) {
+            Box(
+                modifier = Modifier.padding(it)
+            ) {
+
+            }
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun ScreenPreview() {
+    MaterialAppTheme {
+        ScreenContent()
+    }
+}
