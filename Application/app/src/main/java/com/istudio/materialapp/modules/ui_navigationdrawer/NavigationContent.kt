2058a58418c1b75de8a3bda087ea9a8ff81ab787
Devrath <devrath.dev595@gmail.com>
2023-10-19 12:44:31 +0530

    Added images

---
+++ Application/app/src/main/java/com/istudio/materialapp/modules/ui_navigationdrawer/NavigationContent.kt
@@ -0,0 +1,163 @@
+package com.istudio.materialapp.modules.ui_navigationdrawer
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Android
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Android
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+
+data class NavigationData(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unSelectedIcon: ImageVector
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationContent() {
+
+    val cxt = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
+    val items = listOf(
+        NavigationData(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unSelectedIcon = Icons.Outlined.Home
+        ),
+        NavigationData(
+            title = "Profile",
+            selectedIcon = Icons.Filled.Android,
+            unSelectedIcon = Icons.Outlined.Android
+        ),
+        NavigationData(
+            title = "Settings",
+            selectedIcon = Icons.Filled.Settings,
+            unSelectedIcon = Icons.Outlined.Settings
+        )
+    )
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        var selectedItemIndex by rememberSaveable { mutableIntStateOf(0) }
+
+        ModalNavigationDrawer(
+            drawerContent = {
+                // Items of the drawer
+                ModalDrawerSheet {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    items.forEachIndexed { index, navigationData ->
+                        NavigationDrawerItem(
+                            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding),
+                            label = {
+                                Text(text = navigationData.title)
+                            },
+                            // Highlighting the item when it is selected
+                            selected = index == selectedItemIndex,
+                            onClick = {
+                                selectedItemIndex = index
+                                coroutineScope.launch {
+                                    drawerState.close()
+                                }
+                            },
+                            icon = {
+                                Icon(
+                                    imageVector = if (selectedItemIndex == index) {
+                                        navigationData.selectedIcon
+                                    } else {
+                                        navigationData.unSelectedIcon
+                                    }, contentDescription = navigationData.title
+                                )
+                            }
+                        )
+                    }
+                }
+            },
+            drawerState = drawerState
+        ) {
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                topBar = {
+                    TopAppBar(title = {
+                        Text(text = "Drawer Demo")
+                    },
+                        navigationIcon = {
+                            IconButton(
+                                onClick = {
+                                    coroutineScope.launch {
+                                        drawerState.open()
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Menu,
+                                    contentDescription = "Menu"
+                                )
+                            }
+                        }
+                    )
+                }
+            ) {
+                Box(
+                    modifier = Modifier.padding(it)
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = items[selectedItemIndex].title,
+                            fontSize = 26.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+
+}
